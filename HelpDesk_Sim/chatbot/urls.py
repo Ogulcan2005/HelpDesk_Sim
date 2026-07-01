0ae077d6ef17bfa03da6d78e454e96fc4cba26cd
Mitchel Brederije <113167655+Rexouium@users.noreply.github.com>
2026-07-01 13:12:58 +0200

    Fixing the files for v1.1.0

--- HelpDesk_Sim/chatbot/urls.py
+++ HelpDesk_Sim/chatbot/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', home, name='chatbot_home'),                 # frontend pagina van de chatbot
     path('start/', start_chat, name='chatbot_start'),     # start een nieuw gesprek/scenario
     path('ask/', chatbot_response, name='chatbot_ask'),   # stuur een reactie naar de klant
-    path('reset/', reset_scenario, name='chatbot_reset'), # beëindig het huidige gesprek
     path('reset/', reset_scenario, name='chatbot_reset'), # gooi het huidige gesprek weg, start opnieuw
     path('end/', end_chat, name='chatbot_end'),           # sla het gesprek op en beëindig het
     path('logs/', view_chatlog, name='view_chatlog'),
